6073c7a6196315889bab5845a8de3414c6d7384d
jimjames99 <jim@jimjames.org>
2016-04-23 17:17:36 -0400

    remove old net::http configs.

--- lib/mla_active_duty_status/configuration.rb
+++ lib/mla_active_duty_status/configuration.rb
@@ -15,24 +15,12 @@ module MlaActiveDutyStatus
   end
 
   class Configuration
-    attr_accessor :mla_host, :mla_path, :max_years_for_age_check,
-      :pool_size,
-      :warn_timeout,
-      :open_timeout,
-      :read_timeout,
-      :idle_timeout,
-      :keep_alive
+    attr_accessor :mla_host, :mla_path, :max_years_for_age_check
 
     def initialize
       @max_years_for_age_check = 100
       @mla_host = 'mla.dmdc.osd.mil'
       @mla_path = '/mla/single_record.xhtml'
-      @pool_size = 10
-      @warn_timeout = 0.25
-      @open_timeout = 30
-      @read_timeout = 30
-      @idle_timeout = 300
-      @keep_alive = 300
     end
 
   end
