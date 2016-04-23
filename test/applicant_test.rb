eed8b8d48dbac27c26b0789303a9579f407c05f3
jimjames99 <jim@jimjames.org>
2016-04-23 19:58:05 -0400

    fix tests.

--- test/applicant_test.rb
+++ test/applicant_test.rb
@@ -7,7 +7,7 @@ class ApplicantTest < Minitest::Test
 
   describe 'Applicant' do
     before do
-      @mla = MlaActiveDutyStatus::Applicant.new(last_name: 'James', ssn: '123456789', date_of_birth: '1950-02-02')
+      @mla = MlaActiveDutyStatus::Applicant.new('James', nil, nil, '123456789', '1950-02-02', nil)
     end
 
     after do
@@ -15,13 +15,13 @@ class ApplicantTest < Minitest::Test
     end
 
     def test_create_valid_mla
-      mla = MlaActiveDutyStatus::Applicant.new(last_name: 'James', ssn: '123456789', date_of_birth: '1950-02-02')
+      mla = MlaActiveDutyStatus::Applicant.new('James', nil, nil, '123456789', '1950-02-02', nil)
       assert_equal true, mla.valid?
     end
 
     describe 'last_name validations' do
       def test_invalid_last_name
-        mla = MlaActiveDutyStatus::Applicant.new(last_name: '', ssn: '123456789', date_of_birth: '1950-02-02')
+        mla = MlaActiveDutyStatus::Applicant.new(nil, nil, nil, '123456789', '1950-02-02', nil)
         assert_equal false, mla.valid?
         assert_equal 'last_name must be present', mla.errors.join(',')
       end
@@ -29,14 +29,14 @@ class ApplicantTest < Minitest::Test
 
     describe 'date_of_birth validations' do
       def test_invalid_dob
-        mla = MlaActiveDutyStatus::Applicant.new(last_name: 'James', ssn: '123456789', date_of_birth: '1850-02-02')
+        mla = MlaActiveDutyStatus::Applicant.new('James', nil, nil, '123456789', '1850-02-02', nil)
         assert_equal false, mla.valid?
         assert_equal 'date_of_birth must be within 100 years', mla.errors.join(',')
       end
 
       def test_invalid_dob_years_range
         MlaActiveDutyStatus.configuration.max_years_for_age_check = 10
-        mla = MlaActiveDutyStatus::Applicant.new(last_name: 'James', ssn: '123456789', date_of_birth: '1950-02-02')
+        mla = MlaActiveDutyStatus::Applicant.new('James', nil, nil, '123456789', '1950-02-02', nil)
         assert_equal false, mla.valid?
         assert_equal 'date_of_birth must be within 10 years', mla.errors.join(',')
       end
