1189350a71ef15dbb11d392b659db9a78cb60357
jimjames99 <jim@jimjames.org>
2016-04-24 16:04:50 -0400

    use proper variable name.

--- lib/mla_active_duty_status/client.rb
+++ lib/mla_active_duty_status/client.rb
@@ -7,16 +7,16 @@ module MlaActiveDutyStatus
     include SemanticLogger::Loggable
 
     def self.call_mla_site(mla)
-      agent = Mechanize.new do |agent|
+      browser = Mechanize.new do |agent|
         agent.open_timeout = 3
         agent.read_timeout = 3
         agent.log = Logger.new('mechlog.log')
         agent.user_agent_alias = 'Mac Firefox'
       end
-      agent.agent.http.verify_mode = OpenSSL::SSL::VERIFY_NONE # unless MlaActiveDutyStatus.configuration.ssl_verify
+      browser.agent.http.verify_mode = OpenSSL::SSL::VERIFY_NONE if !MlaActiveDutyStatus.configuration.ssl_verify
       host = MlaActiveDutyStatus.configuration.mla_host
       path = MlaActiveDutyStatus.configuration.mla_path
-      page = agent.get("https://#{host}#{path}")
+      page = browser.get("https://#{host}#{path}")
       mla_form = page.form('myForm')
       mla_form.ssn = mla.ssn
       mla_form.ssn2 = mla.ssn
@@ -24,7 +24,7 @@ module MlaActiveDutyStatus
       mla_form.lastName = mla.last_name
       mla_form.firstName = mla.first_name
       mla_form.middleName = mla.first_name
-      page = agent.submit(mla_form, mla_form.buttons[1])
+      page = browser.submit(mla_form, mla_form.buttons[1])
       pdf = page.body
       return Response.parse_response(pdf), pdf
     end
