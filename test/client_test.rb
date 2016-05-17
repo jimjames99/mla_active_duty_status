c5b3a044f070fa15f846dce37197de93bb269da6
jimjames99 <jim@jimjames.org>
2016-05-17 12:38:01 -0400

    Fix tests.

--- test/client_test.rb
+++ test/client_test.rb
@@ -7,7 +7,7 @@ class ClientTest < Minitest::Test
     # Makes a live call so it needs internet access.
     def test_actual_call
       # TODO check that release and prod have certs.
-      mla = MlaActiveDutyStatus::Applicant.new('James', nil, nil, '123456789', '1950-02-02', nil)
+      mla = MlaActiveDutyStatus::Applicant.new('James', nil, nil, '123456789', '1950-02-02')
       # Turn off cert checks in test because Travis will not have CA certs.
       # TODO Fix this by adding current DoD CA certs to gem.
       MlaActiveDutyStatus.configuration.ssl_verify = false
